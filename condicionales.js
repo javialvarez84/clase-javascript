9ef1fc46e7d55261387062f49e9e170454be2738
Javier Alvarez <javieralvarez1278@gmail.com>
2023-08-30 21:20:59 -0600

    Clase terminada

--- condicionales.js
+++ condicionales.js
@@ -1,4 +1,4 @@
-const nombre = "Ada School"
+/* const nombre = "Ada School"
 let edad = 123;
 let nota = 8;
 
@@ -7,3 +7,32 @@ if(nombre === "Ada School" && nota > 7){
 } else {
     console.log("Nombre no existe");
 }
+ */
+
+/* const A = "positivo";
+const B = "negativo";
+
+let positivo = 1;
+let negativo = -1;
+
+if(positivo > 0 ){
+    console.log("El numero es " + positivo)
+
+} */
+
+
+
+function areaCircunferencia(radio){
+    const pi = 3.1416
+
+    const resultado = pi * radio * radio
+    //console.log(resultado)
+    return resultado
+}
+
+const result = areaCircunferencia(10);
+console.log(result);
+console.log(result);
+console.log(result);
+console.log(result);
+
