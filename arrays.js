b0c8d3b8010557cb889cd2518a0e96ca2a2f59b5
Javier Alvarez <javieralvarez1278@gmail.com>
2023-09-01 21:17:12 -0600

    arrays

---
+++ arrays.js
@@ -0,0 +1,28 @@
+// Arrays
+
+let frutas = []
+
+frutas[0] = "manzanas"
+frutas[1] = "bananas"
+frutas[2] = "peras"
+
+console.log(frutas);
+
+let frutas2 = ["platanos", "peras", "frutillas"]
+console.log(frutas2);
+
+/* let family = [];
+// Miembros de la familia
+family[0] = "Hugo"
+family[1] = "Miguel"
+family[2] = "Oscar"
+family[3] = "Javier"
+
+// mascotas
+
+console.log(family); */
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
